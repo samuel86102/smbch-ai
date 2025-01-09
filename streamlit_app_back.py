abd64b1fd2c8432b6bc108a928028f1bf18dfbf6
samuel <samuel86102@gmail.com>
2025-01-09 14:44:44 +0800

    update

--- streamlit_app_back.py
+++ streamlit_app_back.py
@@ -1,10 +1,8 @@
 import streamlit as st
 
-# from groq import Groq
-from openai import OpenAI
+from groq import Groq
 
-
-# groq_api_key = "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"
+groq_api_key = "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"
 
 
 # Read the system prompt from the external file
@@ -17,7 +15,7 @@ st.set_page_config(page_title="教會AI助手", page_icon="✝️")
 
 # Show title and description.
 st.title("✝️  教會AI助手 ")
-st.write("測試階段，可以問教會資訊(資訊截至2025/01/05)")
+st.write("測試階段，可以問教會資訊(資訊截至12/22)")
 
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
@@ -25,13 +23,9 @@ st.write("測試階段，可以問教會資訊(資訊截至2025/01/05)")
 
 
 # Create an Groq client.
-client = OpenAI(
-    api_key="sk-c155cb6974f1429e879d54b117e3befb", base_url="https://api.deepseek.com"
-)
-# client = Groq(api_key=groq_api_key)
+client = Groq(api_key=groq_api_key)
 # model_name = "llama3-8b-8192"
-# model_name = "gemma2-9b-it"
-model_name = "deepseek-chat"
+model_name = "gemma2-9b-it"
 
 # Create a session state variable to store the chat messages. This ensures that the
 # messages persist across reruns.
@@ -52,7 +46,9 @@ for message in st.session_state.messages:
 # Create a chat input field to allow the user to enter a message. This will display
 # automatically at the bottom of the page.
 if prompt := st.chat_input("What is up?"):
-    st.session_state.messages.append({"role": "user", "content": f"{prompt}"})
+    st.session_state.messages.append(
+        {"role": "user", "content": f"reply in zh-tw {prompt}"}
+    )
     st.chat_message("user").write(prompt)
     # response = client.chat.completions.create(model="llama3-8b-8192", messages=st.session_state.messages)
     response = client.chat.completions.create(
