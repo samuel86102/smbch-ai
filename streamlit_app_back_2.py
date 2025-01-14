bb0855ea9966c493206bc9ce957004eb63a473da
samuel <samuel86102@gmail.com>
2025-01-15 01:21:09 +0800

    update

--- streamlit_app_back_2.py
+++ streamlit_app_back_2.py
@@ -1,11 +1,19 @@
 import streamlit as st
+
+# from groq import Groq
 from openai import OpenAI
+
+
+# groq_api_key = "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"
+
 
 # Read the system prompt from the external file
 with open("sys_prompt.txt", "r", encoding="utf-8") as file:
     sys_prompt = file.read()
 
+
 st.set_page_config(page_title="教會AI助手", page_icon="✝️")
+
 
 # Show title and description.
 st.title("✝️  教會AI助手 ")
@@ -16,18 +24,28 @@ st.write("""
 - 可以詢問如何讀聖經
 """)
 
-# Create an OpenAI client.
+# Ask user for their OpenAI API key via `st.text_input`.
+# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
+# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
+
+
+# Create an Groq client.
 client = OpenAI(
     api_key="sk-c155cb6974f1429e879d54b117e3befb", base_url="https://api.deepseek.com"
 )
+# client = Groq(api_key=groq_api_key)
+# model_name = "llama3-8b-8192"
+# model_name = "gemma2-9b-it"
 model_name = "deepseek-chat"
 
 # Create a session state variable to store the chat messages. This ensures that the
 # messages persist across reruns.
+
 if "messages" not in st.session_state:
     st.session_state.messages = [
         {"role": "system", "content": sys_prompt}
     ]  # Add the system prompt here
+
 
 # Display the existing chat messages via `st.chat_message`, excluding the system prompt
 for message in st.session_state.messages:
@@ -35,26 +53,16 @@ for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
+
 # Create a chat input field to allow the user to enter a message. This will display
 # automatically at the bottom of the page.
 if prompt := st.chat_input("平安！我能協助你什麼？"):
     st.session_state.messages.append({"role": "user", "content": f"{prompt}"})
     st.chat_message("user").write(prompt)
-
-    # Start streaming response
+    # response = client.chat.completions.create(model="llama3-8b-8192", messages=st.session_state.messages)
     response = client.chat.completions.create(
-        model=model_name, messages=st.session_state.messages, stream=True
+        model=model_name, messages=st.session_state.messages, stream=False
     )
-    msg = ""
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        for chunk in response:  # Stream the response chunks
-            chunk_content = (
-                chunk.choices[0].delta.content
-                if hasattr(chunk.choices[0].delta, "content")
-                else ""
-            )
-            msg += chunk_content
-            message_placeholder.markdown(msg)  # Update content incrementally
+    msg = response.choices[0].message.content
     st.session_state.messages.append({"role": "assistant", "content": msg})
-
+    st.chat_message("assistant").write(msg)
